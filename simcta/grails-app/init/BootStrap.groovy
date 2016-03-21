7634481e82d2fdc35bd0cef7b807c3f4dca6e236
Emiliemorais <emilie.mo@hotmail.com>
2016-03-21 11:50:24 -0300

    Issue #04 - Setting spring security

--- simcta/grails-app/init/BootStrap.groovy
+++ simcta/grails-app/init/BootStrap.groovy
@@ -1,6 +1,25 @@
+import com.simcta.Role
+import com.simcta.User
+import com.simcta.UserRole
+
 class BootStrap {
 
+    def springSecurityService
+
     def init = { servletContext ->
+    	
+	   	def directorRole = new Role('ROLE_DIRECTOR').save()
+        
+        def me = new User('admin', 'admin').save()
+
+        UserRole.create me, directorRole
+
+        UserRole.withSession {
+            it.flush()
+            it.clear()
+        }
+
+
     }
     def destroy = {
     }
