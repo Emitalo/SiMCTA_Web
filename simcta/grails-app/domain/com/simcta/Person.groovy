4d778d102caa0cc6f2add1a466b7863c1abcc003
Italo Paiva <italosensei@hotmail.com>
2016-03-25 14:10:09 -0300

    Issue #1 - Validating CPF and changing the fields names to Portuguese

--- simcta/grails-app/domain/com/simcta/Person.groovy
+++ simcta/grails-app/domain/com/simcta/Person.groovy
@@ -4,7 +4,7 @@ abstract class Person {
 
 	String name
 	String cpf
-	String rg
+    String rg
 	Date birthdate
 	Address address
 	Phone principalPhone
@@ -17,13 +17,51 @@ abstract class Person {
 
     static constraints = {
     	name blank: false, matches: "[a-zA-Z À-ú_]+"
-    	cpf nullable: false, unique: true
-    	rg nullable: false, matches: "[0-9]+"
-    	email email: true
-    	address nullable: false
-    	principalPhone nullable: false
-    	motherName blank: false
-    	fatherName nullable: true, blank: true
+        cpf editable:false, nullable: false, unique: true, validator:{ Person.validCpf(it)}
+        rg nullable: false, matches: "[0-9]+"
+        email unique: true, email: true
+        address nullable: false
+        principalPhone nullable: false
+        motherName blank: false
+        fatherName nullable: true, blank: true
+        id display: false
+        version display: false
+    }
+
+    static mapping = {
+        tablePerHierarchy false
+    }
+
+    private static def validCpf = {cpf -> 
+        try{
+            int d1, d2
+            int dg1, dg2, remaining
+            int dgcpf
+
+            d1 = d2 = 0
+            dg1 = dg2 = remaining = 0
+
+            for (int i = 1; i < cpf.length() -1; i++) {
+                dgcpf = cpf.substring(i -1, i) as Integer
+                d1 = d1 + ( 11 - i ) * dgcpf
+                d2 = d2 + ( 12 - i ) * dgcpf
+            }
+
+            remaining = (d1 % 11)
+            dg1 = (remaining < 2) ? 0 : 11 - remaining
+
+            d2 += 2 * dg1
+            remaining = (d2 % 11)
+            dg2 = (remaining < 2) ? 0 : 11 - remaining
+
+            def dgverif = cpf.substring(cpf.length()-2)
+            def dgresult = (dg1 as String)  + (dg2 as String)
+
+            return dgverif == dgresult
+
+        }catch (Exception e){
+            return false
+        }
     }
 }
 
@@ -36,18 +74,24 @@ class Address {
 	String city
 
     static constraints = {
-    	addressInfo blank: false
-  		number blank: false
-  		complement blank: true, nullable: true
-  		cep blank: false, maxSize: 8
-  		city blank: false, matches: "[a-zA-Z À-ú_]+"
         id display: false
         version display: false
+        addressInfo blank: false
+        number blank: false
+        complement blank: true, nullable: true
+        cep blank: false, maxSize: 8
+        city blank: false, matches: "[a-zA-Z À-ú_]+"
     }
 
     public String toString(){
 
-        def address = addressInfo + " Nº " + number + " - " + city
+        def address = addressInfo + " Nº " + number + " - "
+
+        if(complement){
+            address += complement + " - "
+        }
+
+        address += city
 
         return address 
     }
@@ -59,7 +103,9 @@ class Phone {
 	String number
 
     static constraints = {
-    	ddd nullable: false, matches: "[0-9]+", validator:{
+        id display: false
+        version display: false
+        ddd nullable: false, matches: "[0-9]+", validator:{
             return it.size() == 2
         }
 
@@ -67,12 +113,11 @@ class Phone {
             return (it.size() >= 8 && it.size() <= 9)
         }
 
-        id display: false
-        version display: false
     }
 
     public String toString(){
         def phone = "(" + ddd + ") " + number
+
         return phone; 
     }
 }
