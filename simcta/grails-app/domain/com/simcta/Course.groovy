1bed6bed1dda890975f17d65ef480c2221186aba
Emiliemorais <emilie.mo@hotmail.com>
2016-03-26 16:02:57 -0300

    Issue #02 - Creating a search bar for courses

--- simcta/grails-app/domain/com/simcta/Course.groovy
+++ simcta/grails-app/domain/com/simcta/Course.groovy
@@ -4,7 +4,16 @@ class Course extends ServiceItem{
 
 	String description
 
+    //static hasMany = [classes: Clas, packge: Packge]
+	//static belongsTo = Packge
+    
+
     static constraints = {	
     	description blank:true, nullable:true
+    	//packge nullable:true
+    }
+
+    String toString(){
+    	return this.name
     }
 }
